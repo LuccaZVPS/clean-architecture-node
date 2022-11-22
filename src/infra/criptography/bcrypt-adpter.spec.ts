92dc540e2536bd7bcecd36543bd10a8bf8af241e
Lucca <luccazvps@gmail.com>
2022-11-22 17:05:18 -0300

    test:ensure that BcryptAdapter return hashed password

--- src/infra/criptography/bcrypt-adpter.spec.ts
+++ src/infra/criptography/bcrypt-adpter.spec.ts
@@ -1,11 +1,21 @@
 import bcryptjs from 'bcryptjs'
 
 import { BcryptAdapter } from './bcrypt-adapter'
+jest.mock('bcryptjs', () => ({
+  async hash (): Promise<string>{
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct values ', async () => {
     const sut = new BcryptAdapter()
     const encrypter = jest.spyOn(bcryptjs, 'hash')
     await sut.encrypt('any_value')
     expect(encrypter).toHaveBeenCalledWith('any_value', expect.anything())
+  })
+  test('should return a hashed password ', async () => {
+    const sut = new BcryptAdapter()
+    const hashedPassword = await sut.encrypt('any_value')
+    expect(hashedPassword).toEqual('hash')
   })
 })
