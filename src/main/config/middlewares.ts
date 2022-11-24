796410a4550baf82b723f61ae8c581f2cdfa3aae
Lucca <luccazvps@gmail.com>
2022-11-24 09:06:54 -0300

    feat: ensure that cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import json from './middlewares/body-parser'
 import urlencoded from './middlewares/url-encoded'
+import { cors } from './middlewares/cors'
 export default (app: Express): void => {
   app.use(json())
   app.use(urlencoded({ extended: true }))
+  app.use(cors)
 }
