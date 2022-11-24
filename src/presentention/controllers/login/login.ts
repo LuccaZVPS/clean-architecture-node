9cf270dbf054f78aa8bcfe18a44df7744bdbfa97
Lucca <luccazvps@gmail.com>
2022-11-24 16:36:39 -0300

    feat: ensure that SignInController return 500 if emailValidator throws

--- src/presentention/controllers/login/login.ts
+++ src/presentention/controllers/login/login.ts
@@ -1,21 +1,25 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok } from '../../helpers/http-helper'
+import { badRequest, internalError, ok } from '../../helpers/http-helper'
 import { Controller, httpRequest, httpResponse } from '../../protocols/index'
 import { EmailValidator } from '../signUp/signUp-protocols'
 export class SignInController implements Controller{
   constructor (private readonly emailValidator: EmailValidator){}
   async handle (httpRequest: httpRequest): Promise<httpResponse>{
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return badRequest(new MissingParamError('email'))
+      }
+      if (!password) {
+        return badRequest(new MissingParamError('password'))
+      }
+      const validEmal = this.emailValidator.isValid(email)
+      if (!validEmal) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return ok({ sucess: true })
+    } catch {
+      return internalError()
     }
-    if (!password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    const validEmal = this.emailValidator.isValid(email)
-    if (!validEmal) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return ok({ sucess: true })
   }
 }
