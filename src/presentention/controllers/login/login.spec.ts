aecdcdd30837cd963c2ce5af4cb302aefabb8ac7
Lucca <luccazvps@gmail.com>
2022-11-24 15:34:40 -0300

    test: ensure that SignInController returns 400 if invalid email is provided

--- src/presentention/controllers/login/login.spec.ts
+++ src/presentention/controllers/login/login.spec.ts
@@ -1,8 +1,30 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { EmailValidator } from '../signUp/signUp-protocols'
 import { SignInController } from './login'
 
 describe('Login Controller', () => {
+  interface ISut {
+    sut: SignInController
+    emailValidator: EmailValidator
+  }
+  const makeEmailValidator = (): EmailValidator => {
+    class FakeEmailValidator implements EmailValidator{
+      isValid (email: string): boolean{
+        return true
+      }
+    }
+    return new FakeEmailValidator()
+  }
+
+  const makeSut = (): ISut => {
+    const fakeEmailValidator = makeEmailValidator()
+    return {
+      sut: new SignInController(fakeEmailValidator),
+      emailValidator: fakeEmailValidator
+    }
+  }
   test('should return 400 if email is not provided', async () => {
-    const sut = new SignInController()
+    const { sut } = makeSut()
     const fakeAccount = {
       body: {
         email: '',
@@ -12,9 +34,10 @@ describe('Login Controller', () => {
     }
     const response = await sut.handle(fakeAccount)
     expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('email'))
   })
   test('should return 400 if password is not provided', async () => {
-    const sut = new SignInController()
+    const { sut } = makeSut()
     const fakeAccount = {
       body: {
         email: 'email@gmail.com',
@@ -24,5 +47,21 @@ describe('Login Controller', () => {
     }
     const response = await sut.handle(fakeAccount)
     expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('should return 400 if email is invalid', async () => {
+    const { sut, emailValidator } = makeSut()
+    jest.spyOn(emailValidator, 'isValid').mockReturnValueOnce(false)
+    const fakeAccount = {
+      body: {
+        email: 'invalid email',
+        password: 'valid_password'
+      }
+
+    }
+    const response = await sut.handle(fakeAccount)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new InvalidParamError('email'))
   })
 })
