ffea69eecaec3354dde9c5ef22d9d0826a347cd3
Lucca <luccazvps@gmail.com>
2022-11-24 10:29:50 -0300

    feat: ensure signup routes return an account on sucess

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import middlewares from './middlewares'
+import setupRoutes from './routes'
 const app = express()
 middlewares(app)
+setupRoutes(app)
 export default app
