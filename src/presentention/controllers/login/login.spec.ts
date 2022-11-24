7911c66ac8b728f80687c3cee30e102792f6f17d
Lucca <luccazvps@gmail.com>
2022-11-24 15:13:34 -0300

    test: ensure that login returns 400 if email is not provided

---
+++ src/presentention/controllers/login/login.spec.ts
@@ -0,0 +1,16 @@
+import { SignInController } from './login'
+
+describe('Login Controller', () => {
+  test('should return 400 if email is not provided', async () => {
+    const sut = new SignInController()
+    const fakeAccount = {
+      body: {
+        email: '',
+        password: 'valid_password'
+      }
+
+    }
+    const response = await sut.handle(fakeAccount)
+    expect(response.statusCode).toBe(400)
+  })
+})
