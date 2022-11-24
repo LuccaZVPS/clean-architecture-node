4aa78f08837a530abbd23b7bf99fc54db4608439
Lucca <luccazvps@gmail.com>
2022-11-24 15:18:37 -0300

    test: ensure that login returns 400 if password is not provided

--- src/presentention/controllers/login/login.spec.ts
+++ src/presentention/controllers/login/login.spec.ts
@@ -13,4 +13,16 @@ describe('Login Controller', () => {
     const response = await sut.handle(fakeAccount)
     expect(response.statusCode).toBe(400)
   })
+  test('should return 400 if password is not provided', async () => {
+    const sut = new SignInController()
+    const fakeAccount = {
+      body: {
+        email: 'email@gmail.com',
+        password: ''
+      }
+
+    }
+    const response = await sut.handle(fakeAccount)
+    expect(response.statusCode).toBe(400)
+  })
 })
