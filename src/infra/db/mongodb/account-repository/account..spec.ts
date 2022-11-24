07af3a9094507df42af91b1d04e6110410da1202
Lucca <luccazvps@gmail.com>
2022-11-24 07:48:36 -0300

    refactor: clear db before each test

--- src/infra/db/mongodb/account-repository/account..spec.ts
+++ src/infra/db/mongodb/account-repository/account..spec.ts
@@ -4,13 +4,21 @@ describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('should return an account on sucess', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'lucca',
       email: 'lucca@gmail.com',
